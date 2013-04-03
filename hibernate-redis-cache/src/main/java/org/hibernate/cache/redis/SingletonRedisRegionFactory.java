f27f131fa4e5c16be8a39322b4aacc3cb2f10a40
debop <sunghyouk.bae@gmail.com>
2013-04-03 19:52:21 +0900

    hibernate-redis, hibernate-search-redis 모듈 추가

--- hibernate-redis-cache/src/main/java/org/hibernate/cache/redis/SingletonRedisRegionFactory.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.cache.redis;
-
-/**
- * org.hibernate.cache.redis.SingletonRedisRegionFactory
- * User: sunghyouk.bae@gmail.com
- * Date: 12. 9. 23.
- */
-public class SingletonRedisRegionFactory {
-}
