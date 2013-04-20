a3723d8f8865d9179222526505273f39f8698910
debop <sunghyouk.bae@gmail.com>
2013-04-20 16:48:09 +0900

    copyright 추가

--- hibernate-redis/src/main/java/org/hibernate/cache/redis/regions/RedisTransactionalDataRegion.java
+++ hibernate-redis/src/main/java/org/hibernate/cache/redis/regions/RedisTransactionalDataRegion.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.hibernate.cache.redis.regions;
 
 import lombok.extern.slf4j.Slf4j;
@@ -84,13 +100,21 @@ public class RedisTransactionalDataRegion extends RedisDataRegion implements Tra
         redis.deleteRegion(getName());
     }
 
-    public void writeLock(Object key) { }
+    public void writeLock(Object key) {
+        // nothing to do.
+    }
 
-    public void writeUnlock(Object key) { }
+    public void writeUnlock(Object key) {
+        // nothing to do.
+    }
 
-    public void readLock(Object key) { }
+    public void readLock(Object key) {
+        // nothing to do.
+    }
 
-    public void readUnlock(Object key) { }
+    public void readUnlock(Object key) {
+        // nothing to do.
+    }
 
     public void evict(Object key) throws CacheException {
         if (log.isTraceEnabled())
--- hibernate-redis/src/main/java/org/hibernate/cache/redis/util/RedisTool.java
+++ hibernate-redis/src/main/java/org/hibernate/cache/redis/util/RedisTool.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.hibernate.cache.redis.util;
 
 import com.google.common.base.Function;
@@ -17,7 +33,7 @@ import java.util.Collection;
 import java.util.Properties;
 
 /**
- * org.hibernate.cache.redis.util.RedisTool
+ * Redis 관련 Utility Method
  *
  * @author sunghyouk.bae@gmail.com
  * @since 13. 4. 4. 오전 10:18
