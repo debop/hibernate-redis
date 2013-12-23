f04db826ef0f08fa1e0782d861997f753f491f7c
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-12-23 23:12:21 +0900

    -. ehcache 비교를 위해 추가.

---
+++ hibernate-redis/src/test/java/org/hibernate/test/ehcache/EhcacheConfiguration.java
@@ -0,0 +1,13 @@
+package org.hibernate.test.ehcache;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * org.hibernate.test.ehcache.EhcacheConfiguration
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 2013. 12. 23. 오후 11:08
+ */
+@Configuration
+public class EhcacheConfiguration {
+}
