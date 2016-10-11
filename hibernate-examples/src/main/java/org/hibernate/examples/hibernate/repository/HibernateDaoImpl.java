913082e87bc080e757c93e883a0fa17b585c8279
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-10-11 13:24:26 +0900

    -. migrate build to gradle to maven ( for build sources artifact )

--- hibernate-examples/src/main/java/org/hibernate/examples/hibernate/repository/HibernateDaoImpl.java
+++ hibernate-examples/src/main/java/org/hibernate/examples/hibernate/repository/HibernateDaoImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.hibernate.examples.hibernate.repository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -215,9 +231,9 @@ public class HibernateDaoImpl implements HibernateDao {
     long totalItemCount = count(countCriteria);
 
     List<T> items = find(clazz, criteria,
-        pageable.getPageNumber() * pageable.getPageSize(),
-        pageable.getPageSize(),
-        CriteriaTool.toOrders(pageable.getSort()).toArray(new Order[0]));
+                         pageable.getPageNumber() * pageable.getPageSize(),
+                         pageable.getPageSize(),
+                         CriteriaTool.toOrders(pageable.getSort()).toArray(new Order[0]));
     return new PageImpl<T>(items, pageable, totalItemCount);
   }
 
@@ -363,9 +379,9 @@ public class HibernateDaoImpl implements HibernateDao {
   @Override
   public long count(Query query, HibernateParameter... parameters) {
     return (Long) HibernateTool.setParameters(query, parameters)
-        .setResultTransformer(CriteriaSpecification.PROJECTION)
-        .setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY)
-        .uniqueResult();
+                               .setResultTransformer(CriteriaSpecification.PROJECTION)
+                               .setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY)
+                               .uniqueResult();
   }
 
   @Override
--- hibernate4/src/test/java/org/hibernate/cache/redis/jpa/JpaCacheTest.java
+++ hibernate4/src/test/java/org/hibernate/cache/redis/jpa/JpaCacheTest.java
@@ -26,7 +26,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -37,7 +37,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {JpaCacheConfiguration.class})
 @Transactional
 public class JpaCacheTest {
